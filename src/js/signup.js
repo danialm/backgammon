d45f94b776f3cbc5e2d83dec9afd82a5d87c7da6
Danial Moazen <dmoazen@fandango.com>
2017-02-17 13:48:23 -0800

    user can register, login, create games

---
+++ src/js/signup.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+class Signup extends Component {
+  render() {
+    return(
+      <div className='login-page'>
+        <h2>Register</h2>
+        <form onSubmit={this.props.onSubmit}>
+          <p>
+            <label>
+              Email:
+              <input type="text"
+                     name="email"
+                     value={this.props.user.email}
+                     onChange={this.props.onChange} />
+            </label>
+          </p>
+          <p>
+            <label>
+              Password:
+              <input type="password"
+                     name="password"
+                     value={this.props.user.password}
+                     onChange={this.props.onChange} />
+            </label>
+          </p>
+          <p>
+            <label>
+              Confirm Password:
+              <input type="password"
+                     name="confirmPassword"
+                     value={this.props.user.confirmPassword}
+                     onChange={this.props.onChange} />
+            </label>
+          </p>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Signup;
