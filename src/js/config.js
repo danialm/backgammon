1a24f369719c09a637c814df9f9efacdaa912d0a
Danial Moazen <dmoazen@fandango.com>
2017-02-17 17:37:01 -0800

    properly detect the environment

--- src/js/config.js
+++ src/js/config.js
@@ -1,6 +1,26 @@
-const Config = {
-  serverUrl: 'https://guarded-depths-69338.herokuapp.com/'
-  // serverUrl: 'http://localhost:3000/'
-};
+const _Environments = {
+    production:  {
+      serverUrl: 'https://guarded-depths-69338.herokuapp.com/'
+    },
+    development: {
+      serverUrl: 'http://localhost:3000/'
+    }
+}
+
+function getPlatform() {
+  if(window.location.hostname === 'localhost'){
+    return 'development';
+  }
+
+  return 'production';
+}
+
+function getEnvironment() {
+    const platform = getPlatform()
+
+    return _Environments[platform]
+}
+
+var Config = getEnvironment();
 
 export default Config;
