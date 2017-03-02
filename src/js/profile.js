c75c3f300c35d97d35c9908e6f2d040e999f8cec
Danial Moazen <dmoazen@fandango.com>
2017-03-02 15:28:35 -0800

    fix

--- src/js/profile.js
+++ src/js/profile.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link, hashHistory } from 'react-router';
+import { Link } from 'react-router';
 import $ from 'jquery';
 import Config from './config.js';
 import Crier from './crier.js';
